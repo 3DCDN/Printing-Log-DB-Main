6e7cfde30439898473f179d50a16fe9e753b89d5
Rich St Onge <richst.ongeywg@gmail.com>
2021-06-15 14:04:38 -0400

    Lesson 8.5 Saving to the Cloud: Set up the Cloud Firestore database, configure security settings, create a convenience initializer, and create a Dictionary type computed property to prepare data for saving to Cloud Firestore.

---
+++ Printing-Log-DB/Spot.swift
@@ -0,0 +1,70 @@
+//
+//  Spot.swift
+//  Printing-Log-DB
+//
+//  Created by XCodeClub on 2021-06-15.
+//
+
+import Foundation
+import FirebaseFirestore
+import FirebaseAuth
+
+class Spot {
+    
+    var name: String
+    var address: String
+    var averageRating: Double
+    var numberOfReviews: Int
+    var postingUserID: String
+    var documentID: String
+    // documentID will be the name of the document therefore will not be saved
+    var dictionary: [String: Any]{
+        return ["name": name, "address": address, "averageRating": averageRating, "numberOfReviews": numberOfReviews, "postingUserID": postingUserID]
+    }
+    
+    init(name: String,address: String, averageRating: Double, numberOfReviews: Int, postingUserID: String, documentID: String) {
+        self.name = name
+        self.address = address
+        self.averageRating = averageRating
+        self.numberOfReviews = numberOfReviews
+        self.postingUserID = postingUserID
+        self.documentID = documentID
+    }
+    
+    func saveData(completion: @escaping (Bool) -> ()) {
+        let db = Firestore.firestore()
+        // Grab the user ID
+        guard let postingUserID = Auth.auth().currentUser?.uid else {
+            print("ERROR: Could not save data because we don't have a valid posting User ID")
+            return completion(false)
+        }
+        self.postingUserID = postingUserID
+        // Create the dictionary representing the data we want to save
+        let dataToSave: [String: Any] = self.dictionary
+        // if we have a Saved record, we'll have a documentID, otherwise .addDocument will create one
+        if self.documentID == "" { // document is new so we need to create document via .addDocument
+            var ref: DocumentReference? = nil
+            ref = db.collection("spots").addDocument(data: dataToSave) { (error) in
+                guard error == nil
+                else {
+                    print("ERROR: Could not add document \(String(describing: error?.localizedDescription))")
+                    return completion(false)
+                }
+                self.documentID = ref!.documentID
+                print("Added document: \(self.documentID)")
+                completion(true)
+            }
+        } else { // document ID already exists and can save with .setData
+            let ref = db.collection("spots").document(self.documentID)
+            ref.setData(dataToSave) { (error) in
+                guard error == nil
+                else {
+                    print("ERROR: Could not save document \(String(describing: error?.localizedDescription))")
+                    return completion(false)
+                }
+                print("Saved document: \(self.documentID)")
+                completion(true)
+            }
+        }
+    }
+}
