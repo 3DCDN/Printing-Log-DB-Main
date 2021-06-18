7402bf174f86200da6e5715f5e7a0ea859d71be9
Rich St Onge <richst.ongeywg@gmail.com>
2021-06-18 13:45:54 -0400

    convenience initializer for dictionary create file for loading data update spot array and tableview in order to load data from the cloud properly create prepare for segue when transitioning to add new data

--- Printing-Log-DB/SpotListViewController.swift
+++ Printing-Log-DB/SpotListViewController.swift
@@ -8,16 +8,22 @@
 import UIKit
 
 class SpotListViewController: UIViewController {
-    var spots = ["Island Creek Oysters","El Pelon","Shake Shack","Pino's Pizza"]
+    var spots: Spots!
     
     @IBOutlet weak var tableView: UITableView!
     @IBOutlet weak var sortSegmentedControl: UISegmentedControl!
     override func viewDidLoad() {
         super.viewDidLoad()
+        spots = Spots()
         tableView.delegate = self
         tableView.dataSource = self
         configureSegmentedControl()
-
+    }
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        spots.loadData {
+            self.tableView.reloadData()
+        }
     }
     func configureSegmentedControl() {
         // set font colors for segmented control
@@ -29,17 +35,23 @@ class SpotListViewController: UIViewController {
         sortSegmentedControl.layer.borderColor = UIColor.white.cgColor
         sortSegmentedControl.layer.borderWidth = 1.0
     }
- 
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "ShowDetail" {
+            let destination = segue.destination as! SpotDetailViewController
+            let selectedIndexPath = tableView.indexPathForSelectedRow!
+            destination.spot = spots.spotArray[selectedIndexPath.row]
+        }
+    }
 }
 extension SpotListViewController: UITableViewDelegate, UITableViewDataSource {
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return spots.count
+        return spots.spotArray.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath) as! SpotTableViewCell
-        cell.nameLabel.text = spots[indexPath.row]
+        cell.nameLabel.text = spots.spotArray[indexPath.row].name
         return cell
     }
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
---
+++ Printing-Log-DB/Spots.swift
@@ -0,0 +1,38 @@
+//
+//  Spots.swift
+//  Printing-Log-DB
+//
+//  Created by XCodeClub on 2021-06-18.
+//
+
+import Foundation
+import Firebase
+
+class Spots {
+    var spotArray: [Spot] = []
+    var db: Firestore!
+    
+    init() {
+        db = Firestore.firestore()
+    }
+    func loadData(completed: @escaping () -> ()){
+        db.collection("spots").addSnapshotListener { (querySnapshot, error) in
+            guard error == nil else {
+                print("ERROR: adding the snapshot listenener \(error!.localizedDescription)")
+                return completed()
+            }
+            // clean out the existing array since new data will load
+            self.spotArray = []
+            // there are querySnapshot!.documents.count documents in the snapshot
+            for document in querySnapshot!.documents {
+                // You'll have to make sure you have a dictionary initializer in the singular class
+                let spot = Spot(dictionary: document.data())
+                spot.documentID = document.documentID
+                self.spotArray.append(spot)
+                
+            }
+            completed()
+        }
+    }
+    
+}
