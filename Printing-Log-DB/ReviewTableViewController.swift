b50e7f0a901735d8d9d8adf4ac726198b3a92e76
Rich St Onge <richst.ongeywg@gmail.com>
2021-07-09 16:00:51 -0400

    Enable buttons to change the star rating for the reviews Add Outlet and button collections. Anytime the star button rating changes, the code for the rating updates.

--- Printing-Log-DB/ReviewTableViewController.swift
+++ Printing-Log-DB/ReviewTableViewController.swift
@@ -19,9 +19,22 @@ class ReviewTableViewController: UITableViewController {
     @IBOutlet weak var reviewDateLabel: UILabel!
     @IBOutlet weak var reviewTextView: UITextView!
     @IBOutlet weak var deleteButton: UIButton!
+    @IBOutlet var starButtonCollection: [UIButton]!
     
     var review: Review!
     var spot: Spot!
+    var rating = 0 {
+        didSet {
+            for starButton in starButtonCollection {
+                let imageName = starButton.tag < rating ? "star.fill" : "star"
+                starButton.setImage(UIImage(systemName: imageName), for: .normal)
+                starButton.tintColor = starButton.tag < rating ? .systemRed : .darkText
+            }
+            print("New rating: \(rating)")
+            review.rating = rating
+        }
+    }
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -41,7 +54,7 @@ class ReviewTableViewController: UITableViewController {
         addressLabel.text = spot.address
         reviewTitleField.text = review.title
         reviewTextView.text = review.text
-        // TODO: update for rating
+        rating = review.rating // will update rating stars on load
     }
     func updateFromUserInterface(){
         review.title = reviewTitleField.text!
@@ -78,6 +91,10 @@ class ReviewTableViewController: UITableViewController {
                 print("ERROR: Can't unwind from segue from Review because of saving error...")
             }
         }
+        
+    }
+    @IBAction func startButtonPressed(_ sender: UIButton) {
+        rating = sender.tag + 1
         
     }
     
